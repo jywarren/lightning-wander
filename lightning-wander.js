42cfcdf4193ec6057fce9a1e050c1b408ef36440
jywarren <jeff@unterbahn.com>
2015-09-07 14:00:35 -0400

    initial working version

---
+++ lightning-wander.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function($) {                                                                                                                                
+
+var onCommand = function(term) {
+
+  $('h1').html(term);
+
+  new GoogleImageSearch(term).done(function (data) {
+
+    $('body').css('background-image', 'url(' + data[0].url + ')');
+
+  });
+
+};
+
+if (annyang) {
+
+  // Let's define a command.
+  var commands = {
+    "are *term": onCommand,
+    "is *term": onCommand,
+    "was *term": onCommand,
+    "the *term": onCommand,
+    "a *term": onCommand,
+    "with *term": onCommand,
+    "for *term": onCommand,
+    "about *term": onCommand,
+    "by *term": onCommand,
+    "to *term": onCommand,
+    "from *term": onCommand,
+    "in *term": onCommand,
+    "of *term": onCommand
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening.
+  annyang.start();
+
+  console.log('start');
+
+}
+
+});
+
+/*
+    var showFlickr = function(tag) {
+      var url = '//api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a828a6571bb4f0ff8890f7a386d61975&sort=interestingness-desc&per_page=9&format=json&callback=jsonFlickrApi&tags='+tag;
+      $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        jsonpCallback: 'jsonFlickrApi',
+        contentType: "application/json",
+        dataType: 'jsonp'
+      });
+    };
+*/
